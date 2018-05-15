e4a613fd27cf2e46a42fca56f93e00174106d164
Laurent Dinclaux <laurent@gecka.nc>
2018-05-15 11:34:43 +1100

    Set permissions on destination files

--- install.sh
+++ install.sh
@@ -12,12 +12,14 @@ chown midnite-modbusd:midnite-modbusd /var/lib/midnite-modbusd
 
 cp etc/midnite-modbusd.service /lib/systemd/system
 cp etc/midnite-modbusd.conf.sample /etc
-chmod +x dist/bin/midnite-modbusd
-chmod +x dist/bin/newmodbus
-chmod +x dist/bin/midnite-classic-data.phar
+
 cp dist/bin/midnite-modbusd /usr/local/bin
 cp dist/bin/newmodbus /usr/local/bin
 cp dist/bin/midnite-classic-data.phar /usr/local/bin/midnite-classic-data
+
+chmod +x /usr/local/bin/midnite-modbusd
+chmod +x /usr/local/bin/newmodbus
+chmod +x /usr/local/bin/midnite-classic-data
 
 echo "Copy /etc/midnite-modbusd.conf.sample to /etc/midnite-modbusd.conf and configure it"
 echo "Then service start midnite-modbusd.service"
